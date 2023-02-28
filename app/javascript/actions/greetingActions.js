6e470b80016e6cf17ffa3052e959588e3a5ec307
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-02-28 20:27:32 +0100

    Retrieve the greeting state from the Redux store and dispatch the fetchGreeting action to retrieve the random greeting from the API endpoint.

--- app/javascript/actions/greetingActions.js
+++ app/javascript/actions/greetingActions.js
@@ -1,58 +1,18 @@
-// Define the initial state of the store
-const initialState = {
-  greeting: "",
-  loading: false,
-  error: null
+export const fetchGreeting = () => {
+  return async (dispatch) => {
+    try {
+      const response = await fetch('https://example.com/api/greeting');
+      const data = await response.json();
+      dispatch(setGreeting(data.message));
+    } catch (error) {
+      console.error('Error fetching greeting:', error);
+    }
+  };
 };
 
-// Define the action types
-const FETCH_GREETING_REQUEST = "FETCH_GREETING_REQUEST";
-const FETCH_GREETING_SUCCESS = "FETCH_GREETING_SUCCESS";
-const FETCH_GREETING_FAILURE = "FETCH_GREETING_FAILURE";
-
-// Define the action creator that will fetch the greeting
-const fetchGreeting = () => async (dispatch) => {
-  dispatch({ type: FETCH_GREETING_REQUEST });
-
-  try {
-    const response = await fetch("https://example.com/api/greeting");
-    const data = await response.json();
-
-    dispatch({ type: FETCH_GREETING_SUCCESS, payload: data.greeting });
-  } catch (error) {
-    dispatch({ type: FETCH_GREETING_FAILURE, payload: error.message });
-  }
+export const setGreeting = (greeting) => {
+  return {
+    type: 'SET_GREETING',
+    payload: greeting,
+  };
 };
-
-// Define the reducer that will handle the state changes
-const greetingReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case FETCH_GREETING_REQUEST:
-      return {
-        ...state,
-        loading: true,
-        error: null
-      };
-
-    case FETCH_GREETING_SUCCESS:
-      return {
-        ...state,
-        greeting: action.payload,
-        loading: false,
-        error: null
-      };
-
-    case FETCH_GREETING_FAILURE:
-      return {
-        ...state,
-        loading: false,
-        error: action.payload
-      };
-
-    default:
-      return state;
-  }
-};
-
-// Create the Redux store
-const store = createStore(greetingReducer, applyMiddleware(thunk));
--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,10 +1,10 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { fetchGreeting } from "../actions/greetingActions";
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchGreeting } from '../actions/greetingActions';
 
 const Greeting = () => {
   const dispatch = useDispatch();
-  const { greeting, loading, error } = useSelector((state) => state.greeting);
+  const greeting = useSelector((state) => state.greeting);
 
   useEffect(() => {
     dispatch(fetchGreeting());
@@ -12,9 +12,7 @@ const Greeting = () => {
 
   return (
     <div>
-      {loading && <p>Loading greeting...</p>}
-      {error && <p>Failed to load greeting: {error}</p>}
-      {greeting && <p>Random greeting: {greeting}</p>}
+      <h1>{greeting}</h1>
     </div>
   );
 };
