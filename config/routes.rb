518335fa2a1f4e46901ec90e607e2c3269aa6ae9
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-02-28 17:59:08 +0100

    Add a route for the random_greeting action in config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'api/random_greeting'
+  namespace :api do
+    get :random_greeting
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
