387820c3cf0001a97107b67d8ae7b96b1bfd66d5
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-02-28 19:06:09 +0100

    Use redux store to display the random greeting in your Greeting component.

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,6 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchGreeting } from "../actions/greetingActions";
 
 const Greeting = () => {
-  const [greeting, setGreeting] = useState
-}
+  const dispatch = useDispatch();
+  const { greeting, loading, error } = useSelector((state) => state.greeting);
+
+  useEffect(() => {
+    dispatch(fetchGreeting());
+  }, [dispatch]);
+
+  return (
+    <div>
+      {loading && <p>Loading greeting...</p>}
+      {error && <p>Failed to load greeting: {error}</p>}
+      {greeting && <p>Random greeting: {greeting}</p>}
+    </div>
+  );
+};
+
+export default Greeting;
